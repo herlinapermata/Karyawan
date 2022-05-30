8feff79c66b00c37b4fa37ccea24032ec031e346
herlinapermata <97141434+herlinapermata@users.noreply.github.com>
2022-05-31 00:14:57 +0800

    menambahkan ATAU pada tabel karyawan.php

---
+++ Menambahkan ATAU pada tabel karyawan.php
@@ -0,0 +1,7 @@
+        </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link <?php echo $_GET['page'] == 'karyawan' || $_GET['page'] == 'karyawancreate' ? ' active' : '' ?>" href="?page=karyawan">
+                <span data-feather="users"></span>
+                Data Karyawan
+              </a>
