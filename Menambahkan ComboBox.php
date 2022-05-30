f91f9314cf8ee723f285ac8a24c5279eb58b45ee
herlinapermata <97141434+herlinapermata@users.noreply.github.com>
2022-05-31 00:26:09 +0800

    menambahkan ComboBox.php

---
+++ Menambahkan ComboBox.php
@@ -0,0 +1,7 @@
+<select class="form-select" aria-label="Default select example" name="status_menikah" required>
+                    <option selected>Pilih Status</option>
+                    <option value="Belum Menikah">Belum Menikah</option>
+                    <option value="Menikah Anak 0">Menikah Anak 0</option>
+                    <option value="Menikah Anak 1">Menikah Anak 1</option>
+                    <option value="Menikah Anak 2">Menikah Anak 2</option>
+                </select>
