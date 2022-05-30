50e9bd7a2665a5585ef75669baa6047de240fa39
herlinapermata <97141434+herlinapermata@users.noreply.github.com>
2022-05-30 23:49:43 +0800

    Mengatur menu SideBar

---
+++ Mengatur menu SideBar.php
@@ -0,0 +1,69 @@
+Dasbor
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" file " > </ span >
+                Pesanan
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" shopping-cart " > </ span >
+                Produk
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" pengguna " > </ span >
+                pelanggan
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" bar-chart-2 " > </ span >
+                Laporan
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" layer " > </ span >
+                Integrasi
+              </a> _ _
+            </ li >
+          </ ul >
+
+          < h6  class =" sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted " >
+            <span>Laporan tersimpan</span>
+            <a class="link-secondary" href="#" aria-label="Tambahkan laporan baru">
+              <span data-feather="plus-circle"></span>
+            </a>
+          </ h6 >
+          < ul  class =" nav flex-column mb-2 " >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" file-text " > </ span >
+                Bulan berjalan
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" file-text " > </ span >
+                Kuartal terakhir
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" file-text " > </ span >
+                Keterlibatan sosial
+              </a> _ _
+            </ li >
+            < li  class =" nav-item " >
+              < a  class =" nav-link " href =" # " >
+                < span  data-feather =" file-text " > </ span >
+                Penjualan akhir tahun
+              </a> _ _
+            </ li >
+          </ ul >
+        </ div >
+      </ nav >
